c9eed174af39cff08e80afda3f3f902931ec8d4b
Federico Jácome <fede_rock_85@hotmail.com>
2020-10-30 12:10:37 +0100

    Practica 54 min cliente tienda cupCake

---
+++ js/app.js
@@ -0,0 +1,93 @@
+// mostrar el carro
+
+(function(){
+    const cartInfo = document.getElementById('cart-info');
+    const cart = document.getElementById('cart');
+
+    cartInfo.addEventListener('click', function(){
+        cart.classList.toggle('show-cart');
+    });
+
+
+})();
+
+
+
+//agrer items al carro
+(function(){
+    const cartBtn = document.querySelectorAll('.store-item-icon');
+
+    cartBtn.forEach(btn => {
+        btn.addEventListener('click', e => {
+            //console.log(e.target);
+
+            if(e.target.parentElement.classList.contains('store-item-icon')) {
+                let fullPath = e.target.parentElement.previousElementSibling.src;
+                let pos = fullPath.indexOf('img') + 3;
+                let partPath = fullPath.slice(pos);
+                
+                const item = {};
+                item.img = `img-cart${partPath}`;
+                let name = e.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
+                item.name = name;
+                let price = e.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
+                let finalPrice = price.slice(1).trim();
+                item.price = finalPrice;
+
+                //console.log(item);
+
+                const cartItem = document.createElement('div');
+                cartItem.classList.add('cart-item', 'd-flex', 'justify-content-between', 'text-capitalize', 'my-3');
+
+                cartItem.innerHTML = `
+                <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+                <div class="item-text">
+                
+                <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+                <span>$</span>
+                <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+                </div>
+                <a href="#" id='cart-item-remove' class="cart-item-remove">
+                <i class="fas fa-trash"></i>
+                </a>
+                </div>`;
+
+                //selecccionando el carro
+                const cart = document.getElementById('cart');
+                const total = document.querySelector('.cart-total-container');
+
+                cart.insertBefore(cartItem, total);
+
+                alert('Agregado correctamente');             
+                
+                showTotals();
+            }
+        })
+    })
+
+
+    //cambiar el total
+    function showTotals(){
+        const total = [];
+        const items = document.querySelectorAll('.cart-item-price');
+
+
+        items.forEach(item => {
+            total.push(parseFloat(item.textContent));
+        })
+
+        let totalMoney = total.reduce((total, item) => { 
+            total += item;
+            return total;
+        }, 0)
+
+        totalMoney = totalMoney.toFixed(2);
+
+        document.getElementById('cart-total').textContent = totalMoney;
+        document.querySelector('.item-total').textContent = totalMoney;
+        document.getElementById('item-count').textContent = total.length;
+
+    }
+
+
+})();
